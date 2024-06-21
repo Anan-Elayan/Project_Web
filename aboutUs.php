d8e09981b079d69f1bf5a2b6c86f34d130aa8887
Anan Elayan <alayyananan@gmail.com>
2024-06-21 14:53:40 +0300

    3rd-version

---
+++ aboutUs.php
@@ -0,0 +1,26 @@
+<?php
+include('db.php.inc');
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+
+    <title>About us page</title>
+</head>
+<body>
+<?php my_header();?>
+<mian >
+    <artical>
+        <h1>Welcome to page about as for Birzeit car Rental agency </h1>
+    </artical>
+
+
+</mian>
+<?php my_footer();?>
+</body>
+</html>
